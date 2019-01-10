9d49a92a6cde38ce5053c0f83552f3ae64c09217
Wind.Young <312456073@qq.com>
2019-01-10 14:35:52 +0800

    Create Atoi.cpp

---
+++ String/Atoi.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+int atoi(char* a){
+	if (a == NULL)
+		return NULL;
+
+	int flag = 1;
+
+	if (*a == '+')
+	{
+		a++;
+	}
+	else if (*a == '-'){
+		a++;
+		flag = -1;
+	}
+
+	int i = 0;
+	while(*a != '\0'){
+		i = i * 10 + (*a) - '0';
+		a++; 
+	}
+
+	return flag*i;
+}
+
+int main()
+{
+	char *a = "12314";
+	cout<<atoi(a);
+}
